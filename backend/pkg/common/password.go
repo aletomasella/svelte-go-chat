7e24e1ac2975fa18074db751145988fedea3b294
Alejandro Tomasella <aletomasella09@gmail.com>
2023-11-14 22:23:58 -0300

    feat: Adding function to generate Random Passwords

--- backend/pkg/common/password.go
+++ backend/pkg/common/password.go
@@ -1,6 +1,11 @@
 package common
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rand"
+	"math/big"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func PasswordHash(password string) (string, error) {
 	h, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -14,4 +19,23 @@ func PasswordHash(password string) (string, error) {
 
 func PasswordHashCompare(password string, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func PassGenerate(length int) string {
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
+		"abcdefghijklmnopqrstuvwxyzåäö" +
+		"0123456789")
+	var password string
+	for i := 0; i < length; i++ {
+		bintRand, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+
+		if err != nil {
+			panic(err)
+		}
+
+		intRand := int(bintRand.Int64())
+
+		password += string(chars[intRand%len(chars)])
+	}
+	return password
 }
